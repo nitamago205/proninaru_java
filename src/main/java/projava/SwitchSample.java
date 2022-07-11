e6fda0d168176be1eabe7de512f776c6a1e6720b
二俣雄樹 <futamatayuki@futamatayuukinoMacBook-Air.local>
2022-07-11 10:56:41 +0900

    7.3.4まで

---
+++ src/main/java/projava/SwitchSample.java
@@ -0,0 +1,12 @@
+package projava;
+
+public class SwitchSample {
+    public static void main(String[] args) {
+        var a = 3;
+        switch (a) {
+            case 1, 2 -> System.out.println("one-two");
+            case 3 -> System.out.println("three");
+            default -> System.out.println("four");
+        }
+    }
+}
