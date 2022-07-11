88b3eb422acc53e24e85de3b82b960e7d243811f
nitamago205 <3ejiz428@gmail.com>
2022-07-11 11:42:45 +0900

    7.3.4まで

--- src/main/java/projava/IfSample.java
+++ src/main/java/projava/IfSample.java
@@ -2,7 +2,7 @@ package projava;
 
 public class IfSample {
     public static void main(String[] args) {
-        var a = 9;
+        var a = 10;
         if (a < 3) {
             System.out.println("小さい");
         } else if (a < 7) {
