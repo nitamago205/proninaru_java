d3c4990a42513fd1d1b8317786c3042de40fae18
nitamago205 <3ejiz428@gmail.com>
2022-07-16 16:18:29 +0900

    9.2.4

--- src/main/java/projava/BreakSample.java
+++ src/main/java/projava/BreakSample.java
@@ -3,8 +3,8 @@ package projava;
 public class BreakSample {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println(i);
-            if (i < 3) {
+            if (i != 2) {
+                System.out.println(i);
                 continue;
             }
             System.out.println("finish");
