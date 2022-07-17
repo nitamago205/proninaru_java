28b1342aab96ed09dbfefb5b209af8623a576244
nitamago205 <3ejiz428@gmail.com>
2022-07-17 19:43:18 +0900

    11.1.4

---
+++ src/main/java/projava/InstanceMethodSample.java
@@ -0,0 +1,21 @@
+package projava;
+
+public class InstanceMethodSample {
+    record Student(String name, int englishScore, int mathScore){
+        int average() {
+            return (this.englishScore() + this.mathScore()) / 2;
+        }
+
+        public int maxScore() {
+            return Math.max(englishScore(), mathScore());
+        }
+    }
+
+    public static void main(String[] args) {
+        var kis = new Student("kis", 60, 80);
+        var a = kis.average();
+        System.out.println("平均点は%d点です".formatted(a));
+        int max = kis.maxScore();
+        System.out.println("最高点は%d点です".formatted(max));
+    }
+}
---
+++ src/main/java/projava/StreamSample2.java
@@ -0,0 +1,17 @@
+package projava;
+
+import java.util.List;
+
+public class StreamSample2 {
+    public static void main(String[] args) {
+        var data = List.of("yamato", "kis", "sugiyama");
+
+        var result = 0;
+        for (var s : data) {
+            if (s.length() >= 5) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
